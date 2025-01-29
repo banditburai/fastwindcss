976f96d302d32970f6eb483edcf3b5bb63ec73c6
firefly <promptsiren@gmail.com>
2025-01-29 11:05:24 -0500

    chore: update daisyui to 5.0.0-beta.3
    chore: remove unneeded release wf
    
    chore: remove unneeded release prepare wf

--- packages/@tailwindcss-standalone/scripts/plugins.config.ts
+++ packages/@tailwindcss-standalone/scripts/plugins.config.ts
@@ -2,7 +2,7 @@ export const plugins = [
     {
       name: 'DaisyUI',
       packageName: 'daisyui',
-      version: '^5.0.0-beta.2'
+      version: '^5.0.0-beta.3'
     },   
     // Add more plugins here as needed
   ]
