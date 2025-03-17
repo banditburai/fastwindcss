da908e8ac7f5af16d3f50417c43d831c5a9140b2
firefly <promptsiren@gmail.com>
2025-03-17 09:06:06 -0400

    chore: update daisy to v5.0.4

--- packages/@tailwindcss-standalone/scripts/plugins.config.ts
+++ packages/@tailwindcss-standalone/scripts/plugins.config.ts
@@ -2,7 +2,7 @@ export const plugins = [
     {
       name: 'DaisyUI',
       packageName: 'daisyui',
-      version: '^5.0.0'
+      version: '^5.0.4'
     },   
     // Add more plugins here as needed
   ]
